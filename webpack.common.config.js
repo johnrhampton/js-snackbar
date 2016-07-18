effac614f1f6fe9f44481960d824dc7be91cfee3
johnrhampton <johnrhampton@gmail.com>
2016-07-18 14:12:02 -0400

    common webpack config

---
+++ webpack.common.config.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+
+  loaders: [
+    {
+      loader: 'babel',
+
+      test: /\.js$/,
+
+      exclude: /node_modules/,
+
+      // Options to configure babel with
+      query: {
+        plugins: ['transform-runtime']
+      }
+    },
+
+    {
+      test: /\.html$/,
+      loader: 'file?name=[name].[ext]'
+    },
+
+    {
+      test: /\.sass$/,
+      loader: ExtractTextPlugin.extract('style', 'css!sass')
+    }
+  ]
+};
