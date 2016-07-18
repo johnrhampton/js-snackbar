177e4d7785c07be1ab68e524a540daf8330ee23d
johnrhampton <johnrhampton@gmail.com>
2016-07-18 12:40:58 -0400

    defaults const

---
+++ src/js/defaults.js
@@ -0,0 +1,23 @@
+export const SNACKBAR = {
+  text: '',
+  textColor: '#ffffff',
+  width: 'auto',
+  showAction: true,
+  actionText: 'Dismiss',
+  actionTextColor: '#4caf50',
+  backgroundColor: '#323232',
+  pos: 'bottom-left',
+  duration: 5000,
+  customClass: '',
+  onActionClick: (element) => {
+    element.style.opacity = 0;
+  }
+};
+
+export const INNER_ELEMENT = {
+  margin: 0,
+  padding: 0,
+  fontSize: '14px',
+  fontWeight: 300,
+  lineHeight: '1em'
+};
