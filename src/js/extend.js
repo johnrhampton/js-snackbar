714b02bf78135d4b47c3a6a38982827365f2ea5a
johnrhampton <johnrhampton@gmail.com>
2016-07-18 12:40:40 -0400

    extend module

---
+++ src/js/extend.js
@@ -0,0 +1,32 @@
+// Pure JS Extend
+// http://gomakethings.com/vanilla-javascript-version-of-jquery-extend/
+export const extend = () => {
+  var extended = {};
+  var deep = false;
+  var i = 0;
+  var length = arguments.length;
+
+  if (Object.prototype.toString.call(arguments[0]) === '[object Boolean]') {
+    deep = arguments[0];
+    i++;
+  }
+
+  const merge = (obj) => {
+    for (var prop in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+        if (deep && Object.prototype.toString.call(obj[prop]) === '[object Object]') {
+          extended[prop] = extend(true, extended[prop], obj[prop]);
+        } else {
+          extended[prop] = obj[prop];
+        }
+      }
+    }
+  };
+
+  for (; i < length; i++) {
+    var obj = arguments[i];
+    merge(obj);
+  }
+
+  return extended;
+};
