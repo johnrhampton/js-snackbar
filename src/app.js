a2d7dd1e922bf864661f88bef283797acb095967
johnrhampton <johnrhampton@gmail.com>
2016-07-18 15:10:01 -0400

    making stylesheets a dependency of snackbar module

--- src/app.js
+++ src/app.js
@@ -3,9 +3,6 @@ import {render} from 'react-dom';
 
 import App from './local/App';
 
-// Stylesheets
-require('./assets/styles/snackbar.sass');
-
 if (document.getElementById('snackbar-local')) {
   render(<App />, document.getElementById('snackbar-local'));
 }
