20cfe8836ee69f1b4babdfb5410afb9c95fa6ef0
johnrhampton <johnrhampton@gmail.com>
2016-07-19 14:08:19 -0400

    ACTION_TYPE, NOTIFY_ICON_OVERRIDES const

--- src/snackbar/defaults.js
+++ src/snackbar/defaults.js
@@ -1,14 +1,21 @@
+export const ACTION_TYPE = {
+  TEXT: 'TEXT',
+  CLOSE: 'CLOSE',
+  NONE: 'NONE'
+};
+
 export const SNACKBAR = {
   text: 'Default Text',
   textColor: '#ffffff',
   width: 'auto',
-  showAction: true,
+  actionType: ACTION_TYPE.CLOSE,
   actionText: 'Dismiss',
   actionTextColor: '#4caf50',
   backgroundColor: '#323232',
   pos: 'bottom-left',
   duration: 5000,
   customClass: '',
+  showNotifyIcon: false,
   onActionClick: (element) => {
     element.style.opacity = 0;
   }
@@ -20,4 +27,10 @@ export const INNER_ELEMENT = {
   fontSize: '14px',
   fontWeight: 300,
   lineHeight: '1em'
+};
+
+export const NOTIFY_ICON_OVERRIDES = {
+  fontSize: '26px',
+  fontWeight: '500',
+  lineHeight: '1.3em'
 };
