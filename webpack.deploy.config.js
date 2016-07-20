6d9a07a8f068d8c25cdb7795260599a5a6a4be53
johnrhampton <johnrhampton@gmail.com>
2016-07-20 10:35:54 -0400

    webpack deploy module testing

--- webpack.deploy.config.js
+++ webpack.deploy.config.js
@@ -1,18 +1,18 @@
 'use strict';
-
+const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 /**
  * common webpack config
  */
-const webpack_common = require('./webpack.common.config');
+// const webpack_common = require('./webpack.common.config');
 
 module.exports = {
   context: __dirname + '/src',
 
   entry: [
     // js entry
-    './lib/snackbar.js',
+    './lib/index.js',
 
     // style entry
     './assets/styles/snackbar.scss'
@@ -21,7 +21,30 @@ module.exports = {
   module: {
     preLoaders: [],
 
-    loaders: webpack_common.loaders
+    loaders: [
+      {
+        loader: 'babel',
+
+        test: /\.js$/,
+
+        exclude: /node_modules/,
+
+        /**
+         * babel config options
+         *
+         * transform-runtime: Externalise references to helpers and builtins, automatically polyfilling
+         *    your code without polluting globals. (This plugin is recommended in a library/tool)
+         */
+        query: {
+          plugins: ['transform-runtime']
+        }
+      },
+
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style', 'css!sass')
+      }
+    ]
   },
 
   plugins: [
@@ -29,14 +52,21 @@ module.exports = {
   ],
 
   output: {
-    filename: 'snackbar.js',
+    filename: 'js-snackbar.js',
     path: __dirname + '/dist',
+
+    /**
+     * export the bundle as library (output.library is the name)
+     * Use this, if you are writing a library and want to publish it as single file.
+     */
     library: 'js-snackbar',
+
     /**
      *  means using universal module definition for the final result
      *  this piece of code recognizes the environment and provides a proper bootstrapping mechanism
      */
     libraryTarget: 'umd',
+
     umdNamedDefine: true
   },
 
