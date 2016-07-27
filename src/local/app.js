9d043b94d560413e869cc8e6b6dbfc96e2269540
johnrhampton <johnrhampton@gmail.com>
2016-07-26 20:32:43 -0400

    local app entry point clean up

--- src/local/app.js
+++ src/local/app.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
 
 import StartScreen from './StartScreen';
 
-if (document.getElementById('snackbar-local')) {
-  render(<StartScreen />, document.getElementById('snackbar-local'));
+if (document.getElementById('local-root')) {
+  ReactDOM.render(<StartScreen />, document.getElementById('local-root'));
 }
