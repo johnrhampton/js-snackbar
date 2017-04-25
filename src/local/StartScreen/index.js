ef7526e94505b5ed3a99969b7126961ca3575f35
shawndube <shawn_dube@hotmail.com>
2017-04-25 15:21:05 -0400

    document new available option

--- src/local/StartScreen/index.js
+++ src/local/StartScreen/index.js
@@ -69,7 +69,7 @@ export default class App extends Component {
 
         <div className="mdl-grid">
           <div className="mdl-cell mdl-cell--4-col">
-            <button onClick={this._showSnackBar.bind(this, {text: 'Custom Action Text', pos: 'top-right', backgroundColor: 'rgb(68, 138, 255)', actionText: 'OK', actionType: ACTION_TYPE.TEXT})}
+            <button onClick={this._showSnackBar.bind(this, {text: 'Custom Action Text', pauseOnHover: true, pos: 'top-right', backgroundColor: 'rgb(68, 138, 255)', actionText: 'OK', actionType: ACTION_TYPE.TEXT})}
                     className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored mdl-js-ripple-effect">
               Custom Action Text
             </button>
